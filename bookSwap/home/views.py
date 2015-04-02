4b2c9fdf51e058e66a5a2505d32b9c9c028e5f5f
Alexander Goossens <ag4wj@virginia.edu>
2015-04-02 19:27:14 -0400

    login added

--- bookSwap/home/views.py
+++ bookSwap/home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
 def index(request):
 	return render(request, 'home/index.html')
+
+def login(request):
+	return render(request, 'home/login.html')
